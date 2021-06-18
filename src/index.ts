5f7b20e695998a3a8ba62db9b8c2dbbe1a189229
Tom Ragle <tragle@gmail.com>
2021-06-18 11:33:59 -0400

    Use user id on inserting new expense, not email

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ app.get('/expenses/:expenseId', async (req, res) => {
 app.post('/expenses', parser, async (req: Request, res: Response) => {
   try {
     const expenseData = req.body;
-    expenseData.user_id = expenseData.user_id || req.app.locals.current_user.email;
+    expenseData.user_id = expenseData.user_id || req.app.locals.current_user.id;
     if (!expenseData.user_id) throw "Missing user_id";
     const { amount, description, user_id } = expenseData;
     const expense = new Expense(expenseData);
