16c193ca25f696bdd658f21fe3c7a982093038d5
Tom Ragle <tragle@gmail.com>
2021-06-18 12:13:33 -0400

    Include optional env var for polar file

--- src/authorization.ts
+++ src/authorization.ts
@@ -2,13 +2,15 @@ import { Oso } from 'oso';
 import { User } from './user';
 import { Expense } from './expense';
 
+const AUTH_FILE = process.env.AUTH_FILE || 'src/authorization.polar';
+
 let oso: Oso;
 
 const initOso = async () => {
   const oso = new Oso();
   oso.registerClass(User);
   oso.registerClass(Expense);
-  await oso.loadFile('src/authorization.polar');
+  await oso.loadFile(AUTH_FILE);
   return oso;
 }
 
